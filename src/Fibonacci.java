3a38235349bd2fe55bc24695816362bb34a757b0
mohdzainuddin11 <92495788+mohdzainuddin11@users.noreply.github.com>
2021-12-11 11:44:22 +0800

    Add files via upload

---
+++ src/Fibonacci.java
@@ -0,0 +1,38 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class Fibonacci {
+
+    private static long[]fibonacciStorage;
+    //setting up storage or cache for memoisation - memoisation is an optimization technique used primarily to speed up computer programs by storing the results of expensive function calls and returning the cached result when the same inputs occur again.
+    public static void main(String[] args) {
+
+        //limited to 92nd number
+        Scanner scanner = new Scanner(System.in);
+        int n;
+        System.out.println("Please enter the nth number of Fibonacci Sequence: ");
+        n = scanner.nextInt();
+
+        fibonacciStorage = new long[n+1];
+
+        for (int i = 0; i <= n; i++) {
+            System.out.print(fibonacci(i) + " , ");
+        }
+    }
+
+    private static long fibonacci(int n) {
+        //states the base case to prevent infinite recursion (stack overflow)
+        if (n<=1) {
+            return n;
+        }
+        if(fibonacciStorage[n] !=0) {
+            return fibonacciStorage[n];
+        }
+        //fibonacci nth number = nth-1 number + nth-2 number => formula: n = (n-1)+(n-2)
+        long nthFibonacciNumber = (fibonacci(n-1) + fibonacci(n-2));
+        fibonacciStorage[n] = nthFibonacciNumber;
+        return nthFibonacciNumber;
+
+    }
+
+}
